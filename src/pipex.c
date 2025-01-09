d962e4825c9347ed239ea4baedcd5a56f8a8e385
hebatist <hebaja@yahoo.com.br>
2025-01-09 00:14:45 -0300

    treating input

--- src/pipex.c
+++ src/pipex.c
@@ -1,36 +1,86 @@
 #include "../include/pipex.h"
 
+char	*compose_cmd(char *cmd)
+{
+	char	*paths[] = {"/usr/bin/", "/usr/sbin/"};
+	char	*parsed_cmd;
+	int	i;
+
+	if (cmd[0] != '/' && access(cmd, F_OK) < 0)
+	{
+		i = 0;
+		while (i < 2)
+		{
+			parsed_cmd = ft_strjoin(paths[i], cmd);
+			if (access(parsed_cmd, F_OK) == 0
+			|| access(parsed_cmd, X_OK) == 0)
+				return (parsed_cmd);
+			free(parsed_cmd);
+			i++;
+		}
+		if (errno == ENOENT)
+			ft_printf("command not found: %s\n", cmd);
+		else
+			ft_printf("%s: %s\n", strerror(errno), cmd);
+		return (NULL);
+	}
+	else
+	{
+		if (access(cmd, X_OK) < 0)
+		{
+			ft_printf("%s: %s\n", strerror(errno), cmd);
+			return (NULL);
+		}
+		return (ft_strdup(cmd));
+	}
+}
+
+int	file_exists(char *file_name)
+{
+	if (access(file_name, F_OK) < 0)
+	{
+		perror(file_name);
+		return (0);
+	}
+	return (1);
+}
+
+/*
+int	command_valid(char *command)
+{
+	if (command[0] != '/')
+	{
+		if (access(ft_strjoin("/bin/", command), F_OK) < 0 || access(ft_strjoin("/bin/", command), X_OK) < 0)
+		{
+			perror(command);
+			return (0);
+		}
+	}
+	return (1);
+}
+*/
+
 int	main(int argc, char **argv)
 {
-	char	*file_name;
-	char	*file_content;
-	size_t	file_len;
+	char	*parsed_cmd;
 
-	int	fd[2];
-	int	pid;
-
-	(void)argc;
-
-	file_name = argv[1];
-	file_len = get_file_len(file_name);
-	file_content = (char *)malloc(sizeof(char) * (file_len));
-	get_file_content(file_name, file_content);
-	ft_printf("content -> %s\n", file_content);
-
-	if (pipe(fd) < 0)
+	if (argc == 3)
 	{
-		perror("pipe");
-		exit(0);
+		if (file_exists(argv[1]))
+			ft_printf("%s\n", argv[1]);
+		else
+			exit(EXIT_FAILURE);
+
+		parsed_cmd = compose_cmd(argv[2]);
+
+		if (parsed_cmd)
+		{
+			ft_printf("%s\n", parsed_cmd);
+			free(parsed_cmd);
+		}
+		else
+			exit(EXIT_FAILURE);
 	}
-	pid = fork();
-	if (pid == 0)
-	{
-		close(fd[0]);
-
-	}
-
-	
-
 
 	return (0);
 }
