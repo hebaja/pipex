38df9c450120e9aaa2e7472169ffc6c6ebf0a5a7
hebatist <hebaja@yahoo.com.br>
2025-01-11 22:00:23 -0300

    dealing with adding path

--- src/pipex.c
+++ src/pipex.c
@@ -22,19 +22,50 @@ size_t	cmd_len(char *cmd)
 	return (count);
 }
 
+int	is_path(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '/')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	command_pos(char *str)
+{
+	int	i;
+	int	last;
+
+	i = 0;
+	last = 0;
+	while (str[i] && str[i] != ' ')
+	{
+		if (str[i] == '/')
+			last = i + 1;
+		i++;
+	}
+	return (last);
+}
+
 char	*add_path(char *cmd)
 {
 	int	i;
 	char	*paths[] = {"/usr/bin/", "/usr/sbin/"};
 	char	*built_cmd;
 	char	*abs_cmd;
+	char	*received_cmd;
 
 	if (cmd[0] != '/' && access(cmd, F_OK) < 0)
 	{
 		i = 0;
 		while (i < 2)
 		{
-			built_cmd = ft_strjoin(paths[i], cmd);
+			built_cmd = ft_strjoin(paths[i++], cmd);
 			abs_cmd = ft_substr(built_cmd, 0, cmd_len(built_cmd));
 			if (access(abs_cmd, F_OK) == 0
 			|| access(abs_cmd, X_OK) == 0)
@@ -45,8 +76,6 @@ char	*add_path(char *cmd)
 			}
 			free(built_cmd);
 			free(abs_cmd);
-			built_cmd = NULL;
-			abs_cmd = NULL;
 		}
 	}
 	else
@@ -60,15 +89,18 @@ char	*add_path(char *cmd)
 			abs_cmd = NULL;
 			return (built_cmd);
 		}
-		free(built_cmd);
-		free(abs_cmd);
-		built_cmd = NULL;
-		abs_cmd = NULL;
 	}
+	received_cmd = ft_substr(cmd, 0, cmd_len(cmd));
 	if (errno == ENOENT)
-		ft_printf("command not found: %s\n", cmd);
+		ft_printf("command not found: %s\n", received_cmd);
 	else
-		ft_printf("%s: %s\n", strerror(errno), cmd);
+		ft_printf("%s: %s\n", strerror(errno), received_cmd);
+	//free(built_cmd);
+	//free(abs_cmd);
+	free(received_cmd);
+	built_cmd = NULL;
+	abs_cmd = NULL;
+	received_cmd = NULL;
 	return (NULL);
 }
 
@@ -76,16 +108,22 @@ t_cmd	*cmd_new(char *cmd)
 {
 	t_cmd	*lst_cmd;
 	int	i;
+	int	cmd_pos;
 	
 	lst_cmd = (t_cmd *)malloc(sizeof(t_cmd) * 1);
 	i = 0;
+	cmd_pos = 0;
 	if (cmd[i])
 	{
 		while (cmd[i] == ' ' || cmd[i] == '\t')
 			i++;
-		lst_cmd->cmd = ft_substr(cmd, 0, cmd_len(cmd));
 
-			lst_cmd->args = ft_split(cmd, ' ');
+		cmd_pos = command_pos(cmd);
+
+		// Maybe, verify if there is path and implement add_path here, case it is needed.
+
+		lst_cmd->cmd = ft_substr(cmd, 0, cmd_len(cmd));
+		lst_cmd->args = ft_split(&cmd[cmd_pos], ' ');
 		lst_cmd->next = NULL;
 	}
 	return (lst_cmd);
@@ -164,11 +202,23 @@ int	main(int argc, char **argv)
 		else
 			exit(EXIT_FAILURE);
 		lst_cmd1 = compose_cmd(argv[2]);
+		if (lst_cmd1)
+		{
+			ft_printf("%s\n", lst_cmd1->cmd);
+			print_args(lst_cmd1->args);
+		}
+		else
+		{
+			//cmd_clear(&lst_cmd1);
+			free(file_content);
+			exit(EXIT_FAILURE);
+		}
 		lst_cmd2 = compose_cmd(argv[3]);
-		ft_printf("%s\n", lst_cmd1->cmd);
-		print_args(lst_cmd1->args);
-		ft_printf("%s\n", lst_cmd2->cmd);
-		print_args(lst_cmd2->args);
+		if (lst_cmd2)
+		{
+			ft_printf("%s\n", lst_cmd2->cmd);
+			print_args(lst_cmd2->args);
+		}
 		if (lst_cmd1)
 		{
 			if (pipe(fd) < 0)
