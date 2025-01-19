ffe89971cea219ed936261a170b3749b8e5f090b
Henrique <hebaja@yahoo.com.br>
2025-01-19 18:46:19 -0300

    refactoring to finish phase 1

--- src/pipex.c
+++ src/pipex.c
@@ -40,7 +40,7 @@ void	print_args(char **args)
 	}
 }
 
-int	do_command(int fd[2][2], int input_fd, int output_fd, t_cmd *lst_cmd)
+int	do_command(int **fd, int input_fd, int output_fd, t_cmd *lst_cmd)
 {
 	int	pid;
 
@@ -84,10 +84,10 @@ int	init_files(char *filename_in, char *filename_out, char **file_content, int *
 	return (res);
 }
 
-int	init_pipes(int (*fd)[3][2])
-{
+// int	init_pipes(int (*fd)[3][2])
+// {
 
-}
+// }
 
 /*
 		lst_cmd1 = compose_cmd(argv[2]);
@@ -109,15 +109,92 @@ int	init_pipes(int (*fd)[3][2])
 		}
 */
 
+void	write_to_outfile(int **fd, int out_fd)
+{
+	int	bytes;
+	char	buffer[1024];
+
+	bytes = -1;
+	bytes = read(fd[2][0], buffer, sizeof(buffer) -1);
+	close(fd[2][0]);
+	dup2(out_fd, STDOUT_FILENO);
+	close(out_fd);
+	if (bytes > 0)
+	{
+		buffer[bytes] = '\0';
+		ft_printf("%s", buffer);
+	}
+	else
+		ft_printf("");
+}
+
+void	fetch_file_content(int **fd, char *file_content)
+{
+	write(fd[0][1], file_content, ft_strlen(file_content));
+	close(fd[0][1]);
+}
+
+void	close_fds(int **fd)
+{
+	close(fd[0][0]);
+	close(fd[1][0]);
+	close(fd[1][1]);
+	close(fd[2][1]);
+}
+
+int	**init_fd(int rows, int cols)
+{
+	int	**fd;
+	int	i;
+	int	j;
+
+	i = -1;
+	j = -1;
+	fd = (int **)malloc(sizeof(int *) * rows);
+	if (fd == NULL)
+	{
+		perror("Failed to allocate memory for fd");
+		return (NULL);
+	}
+	while (++i < rows)
+	{
+		fd[i] = (int *)malloc(sizeof(int) * 2);
+		if (fd[i] == NULL)
+		{
+			perror("Failed to allocate memory for pipe");
+			while (++j < i)
+				free(fd[j]);
+			free(fd);
+			return (NULL);
+		}
+	}
+	return (fd);
+}
+
+int	init_pipes(int **fd, int rows)
+{
+	int	i;
+
+	i = -1;
+	while (++i < rows)
+	{
+		if (pipe(fd[i]) < 0)
+		{
+			perror("Failed to create pipe");
+			return (0);
+		}
+	}
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
-	int	fd[3][2];
+	// int	fd[3][2];
+	int	**fd;
 	char	*file_content;
 	t_cmd	*lst_cmd1;
 	t_cmd	*lst_cmd2;
 	int	out_fd;
-	char	buffer[1024];
-	int	bytes;
 	int	i;
 
 	i = -1;
@@ -132,41 +209,27 @@ int	main(int argc, char **argv)
 		// REFACTOR THIS?
 		if (lst_cmd1 && lst_cmd2)
 		{
-			while (++i < 3)
-			{
-				if (pipe(fd[i]) < 0)
-				{
-					perror("pipe");
-					exit(EXIT_FAILURE);
-				}
-			}
-			int pid1 = do_command(fd, fd[0][0], fd[1][1], lst_cmd1);
-			int pid2 = do_command(fd, fd[1][0], fd[2][1], lst_cmd2);
-			bytes = -1;
-			close(fd[0][0]);
-			close(fd[1][0]);
-			close(fd[1][1]);
-			close(fd[2][1]);
-
-			write(fd[0][1], file_content, ft_strlen(file_content));
-			close(fd[0][1]);
-
-			bytes = read(fd[2][0], buffer, sizeof(buffer) -1);
-			close(fd[2][0]);
-			dup2(out_fd, STDOUT_FILENO);
-			close(out_fd);
+			
+			fd = init_fd(3, 2);
+			if (fd == NULL)
+				exit(EXIT_FAILURE);
 
 
-			if (bytes > 0)
-			{
-				buffer[bytes] = '\0';
-				ft_printf("%s", buffer);
-			}
-			else
-				ft_printf("");
+			if (!init_pipes(fd, 3))
+				exit(EXIT_FAILURE);
 
-			waitpid(pid1, NULL, 0);
-			waitpid(pid2, NULL, 0);
+
+			do_command(fd, fd[0][0], fd[1][1], lst_cmd1);
+			do_command(fd, fd[1][0], fd[2][1], lst_cmd2);
+
+			close_fds(fd);
+			
+			fetch_file_content(fd, file_content);
+
+			write_to_outfile(fd, out_fd);
+
+			// waitpid(pid1, NULL, 0);
+			// waitpid(pid2, NULL, 0);
 
 			cmd_clear(&lst_cmd1);
 			cmd_clear(&lst_cmd2);
